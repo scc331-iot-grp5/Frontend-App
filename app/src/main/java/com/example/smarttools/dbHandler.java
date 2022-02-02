b186a8480ad78e73cef6d63e4578224f22bdf979
CameronPugh <cameron.pugh1@hotmail.com>
2022-02-02 18:25:45 +0000

    Http request integration
    USing Volley

---
+++ app/src/main/java/com/example/smarttools/dbHandler.java
@@ -0,0 +1,57 @@
+package com.example.smarttools;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.HashMap;
+import java.util.Map;
+
+public class dbHandler
+{
+    public String loginRequest(String username, String password)
+    {
+        String result = "";
+        String connStr = "https://e4f8-148-88-245-71.ngrok.io/log-in";
+
+        try {
+            URL url = new URL(connStr);
+            HttpURLConnection con = (HttpURLConnection) url.openConnection();
+            con.setRequestMethod("POST");
+            con.setRequestProperty("Content-Type", "application/json; utf-8");
+            con.setRequestProperty("Accept", "application/json");
+            con.setDoOutput(true);
+            String jsonInputString = "{name:"+username+", job: "+password+"}";
+
+            try(OutputStream os = con.getOutputStream()) {
+                byte[] input = jsonInputString.getBytes("utf-8");
+                os.write(input, 0, input.length);
+            }
+
+            try(BufferedReader br = new BufferedReader(
+                    new InputStreamReader(con.getInputStream(), "utf-8"))) {
+                StringBuilder response = new StringBuilder();
+                String responseLine = null;
+                while ((responseLine = br.readLine()) != null) {
+                    response.append(responseLine.trim());
+                }
+
+                return response.toString();
+            }
+
+        }catch (IOException e) {
+            result = e.getMessage();
+        }
+
+        return result;
+    }
+}
