6c13126b08c1353547196ce6c5356833051c3109
CameronPugh <cameron.pugh1@hotmail.com>
2022-02-21 22:18:09 +0000

    Update ExampleUnitTest.java

--- app/src/test/java/com/google/codelabs/mdc/java/shrine/ExampleUnitTest.java
+++ app/src/test/java/com/google/codelabs/mdc/java/shrine/ExampleUnitTest.java
@@ -10,42 +10,8 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
-    @Test
-    public void formatDateAndTime() {
-        assertEquals(4, 2 + 2);
-    }
-    @Test
-    public void addElementToStartofArray() {
-        assertEquals(5, 3 + 2);
-    }
-    @Test
-    public void assignXaxisToData() {
-        assertEquals(4, 2 + 2);
-    }
-    @Test
-    public void retrieveData() {
-        assertEquals(4, 2 + 2);
-    }
-    @Test
-    public void processResponce() {
-        assertEquals(4, 2 + 2);
-    }
-    @Test
-    public void processResponceError() {
-        assertEquals(4, 2 + 2);
-    }
-    @Test
-    public void queryObject() {
-        assertEquals(4, 2 + 2);
-    }
-    @Test
-    public void filtert() {
-        assertEquals(4, 2 + 2);
-    }
-    @Test
-    public void showToolbar() {
-        assertEquals(4, 2 + 2);
-    }
+
+
 
 
 
